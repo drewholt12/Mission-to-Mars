a633d44e8735fd733620778cb8345fb716fe7246
Andrew Holt <drewholt12@comcast.net>
2021-05-18 18:08:29 -0500

    update files

--- scraping.py
+++ scraping.py
@@ -19,8 +19,9 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now(),
-        "hemisphere_image_urls": mars_hemisphers(browser)
+        "hemisphere_image_urls": mars_hemisphers(browser),
+        "last_modified": dt.datetime.now()
+        
     }
 
     # Stop webdriver and return data
